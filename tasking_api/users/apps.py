e7d14967796bb1034d53cc810bc07284448b100e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-30 09:31:57 +0100

    Implement `ready()` method in Django application configuration class to activate signals
    This commit implements the `ready()` method within the Django application configuration class. The `ready()` method is called by Django when the application is ready to be used. In this case, the method is used to import and activate the signals defined in the `users.signals` module. By importing the `users.signals` module within the `ready()` method, the signals defined in that module will be connected and functional when the application is ready. This allows the signals to respond to specific events or triggers within the application.

--- tasking_api/users/apps.py
+++ tasking_api/users/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+    
+#   *  The provided code snippet suggests the implementation of a `ready()` method within a Django application configuration class. 
+
+# * The `ready()` method is called by Django when the application is ready to be used. In this case, the method is being used to import and activate the signals defined in the `users.signals` module.
+
+#  * By importing the `users.signals` module within the `ready()` method, the signals defined in that module will be connected and functional when the application is ready. This allows the signals to respond to specific events or triggers within the application.
+    def ready(self):
+        import users.signals
