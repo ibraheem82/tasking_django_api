f5f861e110a2e4c6bf385423bbe14788b2d30b9e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-30 09:33:06 +0100

    Set 'id' field as read-only in ProfileAdmin
    This commit updates the `ProfileAdmin` class to include a `readonly_fields` attribute set to a tuple containing the field name `'id'`. By specifying `'id'` as a readonly field, it ensures that the `id` field in the `Profile` model will be displayed as read-only in the admin interface.
    
    This prevents users from modifying the `id` field directly through the admin interface, making it read-only for viewing purposes only. The `ProfileAdmin` class is then registered with the `Profile` model in the Django admin site.

--- tasking_api/users/admin.py
+++ tasking_api/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+# *In this class, the readonly_fields attribute is set to a tuple containing the field name 'id'.
+
+# * By specifying 'id' as a readonly field, it means that the id field in the Profile model will be displayed as read-only in the admin interface. This prevents users from modifying the id field directly through the admin interface, making it read-only for viewing purposes only.
+class ProfileAdmin(admin.ModelAdmin):
+  readonly_fields = ('id',)
+
+admin.site.register(Profile, ProfileAdmin)
+
