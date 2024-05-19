4246737e6f96341998055512e9bd174bdef6b939
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-05-19 10:53:47 +0000

    Set Users App's Default Configuration (__init__.py)
    * Sets `default_app_config = 'users.apps.UsersConfig'` in your project's `settings.py`.
    * This establishes `UsersConfig` located in `users/apps.py` as the configuration class for the `users` app.
    * Explicitly defining the default configuration improves code clarity and maintainability.

--- tasking_api/users/__init__.py
+++ tasking_api/users/__init__.py
@@ -1,3 +1,5 @@
 # specifies the default configuration class ('users.apps.UsersConfig') for a Django app.
-default_app_config = 'users.apps.UsersConfig'
+# * 'users.apps.UsersConfig' refers to the configuration class defined in the apps.py file within the 'users' app.
+# The UsersConfig class likely contains settings related to the app (e.g., app name, verbose name, signals, etc.).
+default_app_config = 'users.apps.UsersConfig' # It tells Django which configuration class to use when initializing the app.
 
